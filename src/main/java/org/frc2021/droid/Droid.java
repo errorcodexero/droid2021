6cc457e601e146095a48df26fed384c1a62da40f
Jack (Butch) Griffin <butchg@comcast.net>
2021-02-02 07:26:01 -0800

    changed the year from 2020 to 2021, not sure it matters but wanted to align with the 2021 WPILIB expectations

---
+++ src/main/java/org/frc2021/droid/Droid.java
@@ -0,0 +1,123 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package org.frc2021.droid;
+
+import edu.wpi.first.wpilibj.RobotBase;
+
+import org.xero1425.simulator.engine.ModelFactory;
+import org.xero1425.simulator.engine.SimulationEngine;
+import org.frc2021.droid.automodes.DroidAutoController;
+import org.frc2021.droid.droidsubsystem.DroidRobotSubsystem;
+import org.frc2021.droid.targettracker.TargetTrackerSubsystem;
+import org.frc2021.droid.turret.TurretSubsystem;
+import org.xero1425.base.XeroRobot;
+import org.xero1425.base.actions.Action;
+import org.xero1425.base.alarms.AlarmSubsystem;
+import org.xero1425.base.controllers.AutoController;
+import org.xero1425.misc.BadParameterTypeException;
+import org.xero1425.misc.MessageLogger;
+import org.xero1425.misc.MissingParameterException;
+import org.xero1425.misc.SimArgs;
+
+/**
+ * The VM is configured to automatically run this class, and to call the
+ * functions corresponding to each mode, as described in the TimedRobot
+ * documentation. If you change the name of this class or the package after
+ * creating this project, you must also update the build.gradle file in the
+ * project.
+ */
+public class Droid extends XeroRobot {
+    static private byte[] practice_bot_mac_addr = new byte[] { 0x00, -128, 0x2F, 0x17, -119, -111 };
+
+    Droid() {
+        super(0.02);
+    }
+
+    public String getSimulationFileName() {
+        String ret = SimArgs.InputFileName ;
+        if (ret != null)
+            return ret ;
+
+        return "testmode" ;
+    }
+
+    protected void addRobotSimulationModels() {
+        ModelFactory factory = SimulationEngine.getInstance().getModelFactory() ;
+        factory.registerModel("conveyor", "org.frc2020.models.ConveyorModel");
+        factory.registerModel("droidoi", "org.frc2020.models.DroidOIModel");
+        factory.registerModel("intake", "org.frc2020.models.IntakeModel");
+        factory.registerModel("shooter", "org.frc2020.models.ShooterModel");
+        factory.registerModel("droid_limelight", "org.frc2020.models.DroidLimelightModel") ;
+        factory.registerModel("turret", "org.frc2020.models.TurretModel") ;
+    }
+
+    public String getName() {
+        return "droid";
+    }
+
+    protected AutoController createAutoController() throws MissingParameterException, BadParameterTypeException {
+        return new DroidAutoController(this) ;
+    }
+
+    protected byte[] getPracticeBotMacAddress() {
+        return practice_bot_mac_addr ;
+    }
+
+    protected void hardwareInit() throws Exception {
+        //
+        // We pulled the climber off for the at home challenges.  Set this to true to
+        // enable support for the climber in the software.
+        //
+        boolean climber_attached = false ;
+
+        DroidRobotSubsystem robotsub = new DroidRobotSubsystem(this, climber_attached) ;
+        setRobotSubsystem(robotsub);
+
+        AlarmSubsystem alarms = robotsub.getAlarms() ;
+        alarms.addEntry(30.0, new ClimbAlarm(robotsub.getOI().getGamepad()));
+    }
+
+    protected void enableMessages() {
+        MessageLogger logger = getMessageLogger() ;
+
+        // logger.enableSubsystem(Action.LoggerName) ;
+        // logger.enableSubsystem(XeroPathManager.LoggerName) ;
+        // logger.enableSubsystem(SettingsParser.LoggerName) ;
+        // logger.enableSubsystem(DroidOISubsystem.SubsystemName) ;
+        // logger.enableSubsystem(ConveyorSubsystem.SubsystemName) ;
+        // logger.enableSubsystem(ConveyorSubsystem.SensorLoggerName) ;        
+        // logger.enableSubsystem(GamePieceManipulatorSubsystem.SubsystemName) ;
+        // logger.enableSubsystem(IntakeSubsystem.SubsystemName) ;
+        // logger.enableSubsystem(ShooterSubsystem.SubsystemName) ;
+        // logger.enableSubsystem(ClimberSubsystem.SubsystemName) ;
+        // logger.enableSubsystem(BlinkySubsystem.SubsystemName) ;
+        // logger.enableSubsystem(DroidLimeLightSubsystem.LoggerSubsystemNameName) ;
+        // logger.enableSubsystem(DroidRobotSubsystem.TankdriveLoggerName) ;
+        // logger.enableSubsystem(TargetTrackerSubsystem.SubsystemName) ;
+        // logger.enableSubsystem(TurretSubsystem.SubsystemName) ;
+        // logger.enableSubsystem(XeroPathManager.LoggerName) ;
+        // logger.enableSubsystem(SettingsParser.LoggerName) ;
+        // logger.enableSubsystem(XeroRobot.LoggerName) ;
+
+        logger.enableSubsystem(Action.LoggerName) ;
+        logger.enableSubsystem(TurretSubsystem.SubsystemName) ;
+        logger.enableSubsystem(TargetTrackerSubsystem.SubsystemName) ;
+
+        if (RobotBase.isSimulation()) {
+            // logger.enableSubsystem(SimulationEngine.LoggerName) ;
+            // logger.enableSubsystem("tankdrive_model") ;
+            // logger.enableSubsystem("conveyor_model") ;
+            // logger.enableSubsystem(ConveyorModel.LogBallPosition) ;
+            // logger.enableSubsystem("shooter_model") ;
+        }
+    }
+
+    protected void loadPathsFile() throws Exception {
+        super.loadPathsFile();
+    }
+}
---
+++ src/main/java/org/frc2021/droid/automodes/DroidTestAutoMode.java
@@ -0,0 +1,272 @@
+package org.frc2021.droid.automodes;
+
+import org.frc2021.droid.climber.ClimberMoveAction;
+import org.frc2021.droid.climber.ClimberSubsystem;
+import org.frc2021.droid.droidsubsystem.DroidRobotSubsystem;
+import org.frc2021.droid.gamepiecemanipulator.FireAction;
+import org.frc2021.droid.gamepiecemanipulator.GamePieceManipulatorSubsystem;
+import org.frc2021.droid.gamepiecemanipulator.ShootTestingAction;
+import org.frc2021.droid.gamepiecemanipulator.conveyor.ConveyorEmitAction;
+import org.frc2021.droid.gamepiecemanipulator.conveyor.ConveyorOnAction;
+import org.frc2021.droid.gamepiecemanipulator.conveyor.ConveyorPrepareToEmitAction;
+import org.frc2021.droid.gamepiecemanipulator.conveyor.ConveyorPrepareToReceiveAction;
+import org.frc2021.droid.gamepiecemanipulator.conveyor.ConveyorReceiveAction;
+import org.frc2021.droid.gamepiecemanipulator.conveyor.ConveyorSetBallCountAction;
+import org.frc2021.droid.gamepiecemanipulator.conveyor.ConveyorStopAction;
+import org.frc2021.droid.gamepiecemanipulator.conveyor.ConveyorSubsystem;
+import org.frc2021.droid.gamepiecemanipulator.intake.CollectOffAction;
+import org.frc2021.droid.gamepiecemanipulator.intake.CollectOnAction;
+import org.frc2021.droid.gamepiecemanipulator.intake.IntakeSubsystem;
+import org.frc2021.droid.gamepiecemanipulator.shooter.ShooterSubsystem;
+import org.frc2021.droid.gamepiecemanipulator.shooter.ShooterVelocityAction;
+import org.frc2021.droid.gamepiecemanipulator.shooter.ShooterSubsystem.HoodPosition;
+import org.frc2021.droid.targettracker.TargetTrackerSubsystem;
+import org.frc2021.droid.turret.FollowTargetAction;
+import org.frc2021.droid.turret.TurretSubsystem;
+import org.xero1425.base.actions.DelayAction;
+import org.xero1425.base.actions.ParallelAction;
+import org.xero1425.base.actions.SequenceAction;
+import org.xero1425.base.controllers.AutoMode;
+import org.xero1425.base.controllers.TestAutoMode;
+import org.xero1425.base.motorsubsystem.MotorEncoderGotoAction;
+import org.xero1425.base.motorsubsystem.MotorEncoderPowerAction;
+import org.xero1425.base.motorsubsystem.MotorEncoderVelocityAction;
+import org.xero1425.base.tankdrive.TankDriveFollowPathAction;
+import org.xero1425.base.tankdrive.TankDrivePowerAction;
+import org.xero1425.base.tankdrive.TankDriveScrubCharAction;
+import org.xero1425.base.tankdrive.TankDriveSubsystem;
+
+//
+// This is a test automode.  It is used to run tests on various aspects of
+// the robot.  Each subsystem has a set of tests that ensure the motors are
+// working and to also test higher level actions.  There are also tests to help
+// with the tuning of the robot.
+//
+public class DroidTestAutoMode extends TestAutoMode {
+
+    private ShootTestingAction shoot_ = null ;
+
+    public DroidTestAutoMode(DroidAutoController ctrl)
+            throws Exception {
+        super(ctrl, "Droid-Test-Mode") ;
+
+        ParallelAction parallel ;
+        SequenceAction seq ;
+        
+        DroidRobotSubsystem droid = (DroidRobotSubsystem)ctrl.getRobot().getRobotSubsystem() ;
+        TankDriveSubsystem db = droid.getTankDrive() ;
+        GamePieceManipulatorSubsystem gp = droid.getGamePieceManipulator() ;
+        IntakeSubsystem intake = droid.getGamePieceManipulator().getIntake() ;
+        ConveyorSubsystem conveyor = droid.getGamePieceManipulator().getConveyor() ;
+        ShooterSubsystem shooter = droid.getGamePieceManipulator().getShooter() ;
+        TurretSubsystem turret = droid.getTurret() ;
+        ClimberSubsystem climber = droid.getClimber() ;
+        TargetTrackerSubsystem tracker = droid.getTracker() ;        
+
+        switch(getTestNumber()) {
+            //
+            // Numbers 0 - 9 are for the driverbase
+            //
+            case 0:         // Drive straight, used to test and get Kv number
+                addSubActionPair(db, new TankDrivePowerAction(db, getPower(), getPower(), getDuration()), true);
+                break ;
+
+            case 1:         // Drive curve to the right
+                addSubActionPair(db, new TankDrivePowerAction(db, getPower(), getPower() / 2, getDuration()), true);
+                break ;
+
+            case 2:         // Drive curve to the left
+                addSubActionPair(db, new TankDrivePowerAction(db, getPower() / 2, getPower(), getDuration()), true);
+                break ;
+
+            case 3:         // Rotate robot to get scrub factor
+                addSubActionPair(db, new TankDriveScrubCharAction(db, getPower(), getPosition()), true);
+                break ; 
+
+            case 4:         // Run the path follower to follow a named path
+                addSubActionPair(db, new TankDriveFollowPathAction(db, getNameParam(), false), true) ;
+                break ;
+
+            //
+            // Numbers 10 - 19 are for the intake
+            //
+            case 10:        // Calculate the Kv number
+                addSubActionPair(intake, new MotorEncoderPowerAction(intake, getPower(), getDuration()), true);
+                break ;
+
+            case 11:        // Test to goto position action
+                addSubActionPair(intake, new MotorEncoderGotoAction(intake, getPosition(), true), true) ;
+                break ;                
+
+            case 12:        // Test the high level collect on and collect off action
+                addSubActionPair(intake, new CollectOnAction(intake), true);
+                addAction(new DelayAction(ctrl.getRobot(), 3.0));
+                addSubActionPair(intake, new CollectOffAction(intake), true);
+                break ;
+
+            //
+            // Numbers 20 - 29 are for the conveyor
+            //
+            case 20:            // Run intake side of conveyor in forward direction
+                addSubActionPair(conveyor, new ConveyorOnAction(conveyor, 1.0, 0.0), true);
+                addAction(new DelayAction(ctrl.getRobot(), 3.0));
+                addSubActionPair(conveyor, new ConveyorOnAction(conveyor, 0.0, 0.0), true);
+                break ;
+
+            case 21:            // Run intake side of conveyor in backward direction
+                addSubActionPair(conveyor, new ConveyorOnAction(conveyor, -1.0, 0.0), true);
+                addAction(new DelayAction(ctrl.getRobot(), 3.0));                
+                addSubActionPair(conveyor, new ConveyorOnAction(conveyor, 0.0, 0.0), true);
+                break ;
+
+            case 22:            // Run shooter side of conveyor in forward direction
+                addSubActionPair(conveyor, new ConveyorOnAction(conveyor, 0.0, 1.0), true);                
+                addAction(new DelayAction(ctrl.getRobot(), 3.0));                
+                addSubActionPair(conveyor, new ConveyorOnAction(conveyor, 0.0, 0.0), true);
+                break ;
+
+            case 23:            // Run shooter side of conveyor in backward direction
+                addSubActionPair(conveyor, new ConveyorOnAction(conveyor, 0.0, -1.0), true);
+                addAction(new DelayAction(ctrl.getRobot(), 3.0));                
+                addSubActionPair(conveyor, new ConveyorOnAction(conveyor, 0.0, 0.0), true);
+                break ;
+
+            case 24:            // Run shooter and intake side of conveyor in forward direction
+                addSubActionPair(conveyor, new ConveyorOnAction(conveyor, 1.0, 1.0), true);
+                addAction(new DelayAction(ctrl.getRobot(), 3.0));                
+                addSubActionPair(conveyor, new ConveyorOnAction(conveyor, 0.0, 0.0), true);
+                break ;
+
+            case 25:            // Run shooter and intake side of conveyor in backward direction
+                addSubActionPair(conveyor, new ConveyorOnAction(conveyor, -1.0, -1.0), true);
+                addAction(new DelayAction(ctrl.getRobot(), 3.0));                
+                addSubActionPair(conveyor, new ConveyorOnAction(conveyor, 0.0, 0.0), true);
+                break ;
+
+            case 26:            // Test collect path
+                parallel = new ParallelAction(ctrl.getRobot().getMessageLogger(), ParallelAction.DonePolicy.All) ;
+                addAction(parallel);
+                parallel.addSubActionPair(intake, new CollectOnAction(intake), false);
+                seq = new SequenceAction(ctrl.getRobot().getMessageLogger()) ;
+                parallel.addAction(seq) ;
+                seq.addSubActionPair(conveyor, new ConveyorPrepareToReceiveAction(conveyor), true);
+                seq.addSubActionPair(conveyor, new ConveyorReceiveAction(conveyor), true);
+                break ;
+
+            case 27:            // Test shoot path
+                addSubActionPair(conveyor, new ConveyorPrepareToEmitAction(conveyor), true);
+                addSubActionPair(conveyor, new ConveyorEmitAction(conveyor), true);
+                addAction(new DelayAction(ctrl.getRobot(), 3.0));
+                addSubActionPair(conveyor, new ConveyorStopAction(conveyor), true);
+                break ;
+
+            //
+            // Numbers 30 - 39 are for the shooter
+            //
+            case 30:            // Run the shooter at a fixed power for a fixed duration, gets Kf for velocity
+                addSubActionPair(shooter, new MotorEncoderPowerAction(shooter, getPower(), getDuration()), true);
+                break ;
+
+            case 31:            // Run the base motor encoder velocity action
+                addSubActionPair(shooter, new MotorEncoderVelocityAction(shooter, getPower(), getDuration()), true) ;
+                break ;
+
+            case 32:            // Set shooter to fixed velocity (hood down)
+                addSubActionPair(shooter, new ShooterVelocityAction(shooter, getPower(), HoodPosition.Down), true);
+                break ;
+
+            case 33:            // Set shooter to fixed velocity (hood up)
+                addSubActionPair(shooter, new ShooterVelocityAction(shooter, getPower(), HoodPosition.Up), false);
+                break ;
+                
+            case 34:            // Characterize the shooter, gets velocity from smartdashboard - hood up
+                if (shoot_ == null)
+                    shoot_ = new ShootTestingAction(gp, HoodPosition.Up) ;
+                addSubActionPair(gp, shoot_, true) ;
+                break ;
+
+            case 35:            // Characterize the shooter, gets velocity from smartdashboard - hood down
+                if (shoot_ == null)
+                    shoot_ = new ShootTestingAction(gp, HoodPosition.Down) ;
+                addSubActionPair(gp, shoot_, true) ;
+                break ;
+
+            case 36:            // Test the hood
+                addSubActionPair(shooter, new ShooterVelocityAction(shooter, 0.0, HoodPosition.Up), false);
+                addAction(new DelayAction(ctrl.getRobot(), 3.0));
+                addSubActionPair(shooter, new ShooterVelocityAction(shooter, 0.0, HoodPosition.Up), false);
+                addAction(new DelayAction(ctrl.getRobot(), 3.0));
+                addSubActionPair(shooter, new ShooterVelocityAction(shooter, 0.0, HoodPosition.Down), false);
+                addAction(new DelayAction(ctrl.getRobot(), 3.0));
+                addSubActionPair(shooter, new ShooterVelocityAction(shooter, 0.0, HoodPosition.Up), false);
+                addAction(new DelayAction(ctrl.getRobot(), 3.0));
+                addSubActionPair(shooter, new ShooterVelocityAction(shooter, 0.0, HoodPosition.Down), false);             
+                break ;
+                
+            case 37:            // Manual shoot, hood down
+                addSubActionPair(conveyor, new ConveyorSetBallCountAction(conveyor, 5), false);
+                addSubActionPair(shooter, new ShooterVelocityAction(shooter, getPower(), HoodPosition.Down), false);
+                addAction(new DelayAction(ctrl.getRobot(), 5.0));
+                addSubActionPair(conveyor, new ConveyorPrepareToEmitAction(conveyor), true);
+                addSubActionPair(conveyor, new ConveyorEmitAction(conveyor), true);
+                break ;
+
+            case 38:            // Manual shoot, hood up
+                addSubActionPair(conveyor, new ConveyorSetBallCountAction(conveyor, 5), false);
+                addSubActionPair(shooter, new ShooterVelocityAction(shooter, getPower(), HoodPosition.Up), false);
+                addAction(new DelayAction(ctrl.getRobot(), 5.0));
+                addSubActionPair(conveyor, new ConveyorPrepareToEmitAction(conveyor), true);
+                addSubActionPair(conveyor, new ConveyorEmitAction(conveyor), true);
+                break ;
+
+            //
+            // Numbers 40 - 49 are for the turret
+            //
+            case 40:               // Run the turret at a fixed power for a fixed duration, gets Kv for velocity
+                addSubActionPair(turret, new MotorEncoderPowerAction(turret, getPower(), getDuration()), true);
+                break ;
+
+            case 41:                // Go to specific angle
+                addSubActionPair(turret, new MotorEncoderGotoAction(turret, getPosition(), false), true);
+                break ;
+
+            case 42:                // Follow the target
+                addSubActionPair(turret, new FollowTargetAction(turret, tracker), true);
+                break ;
+                
+            //
+            // Numbers 50 - 59 are for the control panel spinner
+            //                
+            case 50:
+                break ;
+
+            //
+            // Numbers 60 - 69 are for the climber
+            //
+            case 60:
+                addSubActionPair(climber.getLifter(), new MotorEncoderPowerAction(climber.getLifter(), getPower(), getDuration()), true);
+                break ;
+
+            case 61:
+                addSubActionPair(climber.getLifter(), new MotorEncoderGotoAction(climber.getLifter(), getPosition(), true), true);
+                break ;
+
+            case 62:
+                addSubActionPair(climber, new ClimberMoveAction(climber, getPower(), 0.0), true);
+                addAction(new DelayAction(ctrl.getRobot(), getDuration()));
+                addSubActionPair(climber, new ClimberMoveAction(climber, 0.0, 0.0), true);                
+                break ;
+
+            // 100+ Whole robot test modes
+            case 100:   // Complete shooting sequence
+                addSubActionPair(turret, new FollowTargetAction(turret, tracker), false);
+                addSubActionPair(intake, new CollectOnAction(intake), true);
+                addSubActionPair(conveyor, new ConveyorPrepareToReceiveAction(conveyor), true);
+                addSubActionPair(conveyor, new ConveyorReceiveAction(conveyor), true);
+                addSubActionPair(intake, new CollectOffAction(intake), true);   
+                addSubActionPair(conveyor, new ConveyorPrepareToEmitAction(conveyor), true);
+                addSubActionPair(gp, new FireAction(gp, tracker, turret, db), true) ;
+                break ;
+        }
+    }
+}
